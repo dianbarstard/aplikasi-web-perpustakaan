e01e84e6136d094327c9dd0e28d246004a1058e9
dennamandela <denna.mandela13@gmail.com>
2022-05-21 12:27:31 +0700

    Ini projek pertama

---
+++ resources/views/buku.blade.php
@@ -0,0 +1,18 @@
+<html> 
+    <head>
+        <title>contoh</title>
+    </head>
+    <style>
+       body {
+            background-color: blue;
+        }
+
+        h1 {
+            color: red;
+        }
+        
+        </style>
+    <body>
+        <h1> ini adalah comtoh</h1>
+    </body>    
+</html>                               
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+<html> 
+    <head>
+        <title>contoh</title>
+    </head>
+    <body>
+        <h1> ini adalah comtoh</h1>
+        <p>selamat datang di aplikasi web perpustakaan</p>
+    </body>    
+</html>                               
